d864f02e516bc3a4370b0e944d543e584b138a10
刘强 <12829543+jiao-xiao-liu@user.noreply.gitee.com>
2023-07-24 10:39:37 +0800

    first commit

--- src/main/java/com/lq/GitTest.java
+++ src/main/java/com/lq/GitTest.java
@@ -3,5 +3,8 @@ package com.lq;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("git 测试 哈哈哈哈");
+        System.out.println("git 测试 1265558");
+        System.out.println("git 测试5656555");
+
     }
 }
